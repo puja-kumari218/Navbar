281a923ecf2baab10803f806faf4488078b12e32
puja kumari <pkumari41038@gmail.com>
2023-05-14 14:25:34 +0530

    navbar

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+
+
+  // console.log(links.classList.contains("random"));
+  
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else
+
+
+
+
+  links.classList.toggle("show-links");
+});
